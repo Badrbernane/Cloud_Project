538df372521def2ea8d71bc06db0a8459af712ad
unknown <oumabdo622@gmail.com>
2025-12-25 21:56:31 +0100

    model1

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,12 +5,48 @@ import Post from "../components/post"
 import SuggestedUsers from "../components/suggested-users"
 import LiveChat from "../components/live-chat"
 import ProfilePage from "./profile"
+import FantazyPage from "./fantazy"
+import PlayersPage from "./players"
+import MyTeamPage from "./my-team"
+import LeaderboardPage from "./leaderboard"
 
 export default function Home({ onLogout }) {
-  const [showProfile, setShowProfile] = useState(false);
+  const [view, setView] = useState('feed');
 
-  if (showProfile) {
-    return <ProfilePage onLogout={onLogout} onBack={() => setShowProfile(false)} />;
+  const handleNavigate = (destination) => {
+    if (destination === 'profile') {
+      setView('profile');
+    } else if (destination === 'fantazy') {
+      setView('fantazy');
+    } else if (destination === 'players') {
+      setView('players');
+    } else if (destination === 'myteam') {
+      setView('myteam');
+    } else if (destination === 'leaderboard') {
+      setView('leaderboard');
+    } else {
+      setView('feed');
+    }
+  };
+
+  if (view === 'profile') {
+    return <ProfilePage onLogout={onLogout} onBack={() => setView('feed')} onNavigate={handleNavigate} />;
+  }
+
+  if (view === 'fantazy') {
+    return <FantazyPage onLogout={onLogout} onBack={() => setView('feed')} onNavigate={handleNavigate} />;
+  }
+
+  if (view === 'players') {
+    return <PlayersPage onLogout={onLogout} onBack={() => setView('fantazy')} onNavigate={handleNavigate} />;
+  }
+
+  if (view === 'myteam') {
+    return <MyTeamPage onLogout={onLogout} onBack={() => setView('fantazy')} onNavigate={handleNavigate} />;
+  }
+
+  if (view === 'leaderboard') {
+    return <LeaderboardPage onLogout={onLogout} onBack={() => setView('fantazy')} onNavigate={handleNavigate} />;
   }
 
   return (
@@ -23,9 +59,9 @@ export default function Home({ onLogout }) {
         backgroundAttachment: 'fixed'
       }}
     >
-      <Sidebar onLogout={onLogout} />
+      <Sidebar onLogout={onLogout} onNavigate={handleNavigate} />
       <div className="ml-20 p-4">
-        <Navbar onProfileClick={() => setShowProfile(true)} />
+        <Navbar onProfileClick={() => setView('profile')} />
         <div className="flex space-x-4">
           <div className="flex-1">
             <Post
